d456549c11f3800b79c107963647408e9a647eef
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-08-11 07:49:43 +0300

    Add a table

--- src/pages/components/Button.vue
+++ src/pages/components/Button.vue
@@ -5,11 +5,38 @@ q-page(padding)
   section.q-title#introduction Introduction
   p Quasar has a component called QBtn which is a button with a few extra useful features. For instance, it comes in two shapes: rectangle (default) and round. It also has the material ripple effect by default when building with Material theme.
 
-  p Some code markup
+  p.text-red Some code markup
   code-markup(lang="bash")
     | $ quasar dev -m ssr
     | # some comment
     | $ quasar build
+
+  p.q-mt-lg.text-red Some table
+  table.q-mb-lg
+    thead
+      tr
+        th Property
+        th Type
+        th Description
+    tbody
+      tr
+        td color
+        td String
+        td The color the QInput should have. The default is primary.
+      tr
+        td inverted
+        td Boolean
+        td Set to true, to color field&rsquo;s background set by the color prop.
+      tr
+        td dark
+        td Boolean
+        td
+          | Set to true, if the field is on a dark background. It will invert the text color to make it light.
+      tr
+        td align
+        td Text
+        td
+          | Controls the &lsquo;right&rsquo;, &lsquo;center&rsquo; or &lsquo;left&rsquo; alignment of the input. The default is &lsquo;left&rsquo;.
 
   p The button component also comes with a spinner or loading effect. You would use this for times when app execution may cause a delay and you want to give the user some feedback about that delay. When used, the button will display a spinning animation as soon as the user clicks the button.
 
